7d52b7ac186e588b7431567fa9bb82ded5ec7d4f
ygricks <17603514+ygricks@users.noreply.github.com>
2019-12-01 01:41:10 +0200

    create ui-dom elements in js

--- script.js
+++ script.js
@@ -7,20 +7,11 @@ Object.setPrototypeOf(SubDate.prototype, Date.prototype);
 SubDate.prototype.getMyMount = function(){return this.getMonth()+1;};
 SubDate.prototype.addYear = function(y){this.setYear(this.getFullYear()+y); return this;};
 SubDate.prototype.addMonth = function(m){this.setMonth(this.getMonth()+m); return this;};
-SubDate.prototype.addDays = function(d){this.setTime(this.getTime()+(d*24*60*60*1000));return this;};
-SubDate.prototype.addHours = function(h){this.setTime(this.getTime()+(h*60*60*1000));return this;};
-SubDate.prototype.addMinutes = function(m){this.setTime(this.getTime()+(m*60*1000));return this;};
-//---------------------------------------------------------------------------------
-var t = function(n,func){
-	this.func=func;
-	this.v=n;
-	this.val = function(){return (this.v+'').length>2?this.v :(("0" + this.v).slice(-2));}
-};
-function highlight(element){
-	element[0].classList.remove("highlight");
-	void element[0].offsetWidth;
-	element[0].classList.add("highlight");
-}
+SubDate.prototype.addDays = function(d){this.setTime(this.getTime()+(d*86400000));return this;};
+SubDate.prototype.addHours = function(h){this.setTime(this.getTime()+(h*3600000));return this;};
+SubDate.prototype.addMinutes = function(m){this.setTime(this.getTime()+(m*60000));return this;};
+
+
 var MD = function(element, time){
 	const self = this;
 	return self.init(element, time);
@@ -41,6 +32,12 @@ MD.prototype.init = function(element, time){
 	Object.keys(self.list_attr).forEach(function(v){
 		self.list_dom[v] = self.element.querySelectorAll('.e[data-id="'+v+'"] .val')[0];
 	});
+	self.eventsListner();
+	self.createDOM();
+	return self;
+};
+MD.prototype.eventsListner = function(){
+	const self = this;
 	self.element.addEventListener('click', function(event){
 		var t = event.target;
 		var p = function(e){return event.target.parentNode.getAttribute('data-id');};
@@ -58,28 +55,49 @@ MD.prototype.init = function(element, time){
 			self.update(p.getAttribute('data-id'),d);
 		}
 	};
-	console.log('before add list mouse wheel');
-	self.element.addEventListener('mousewheel', scrolled, { passive: false })
-	self.element.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+	self.element.addEventListener('mousewheel', scrolled, { passive: false });
+	self.element.addEventListener('DOMMouseScroll', scrolled, { passive: false });
 	return self;
 };
-// MD.prototype.initAttr = function(){
-// 	const self = this;
-
-// 	return self;
-// };
+MD.prototype.highlight = function(element){
+	element.classList.remove("highlight");
+	void element.offsetWidth;
+	element.classList.add("highlight");
+}
 MD.prototype.update = function(attr, indice){
 	const self = this;
 	self.ts[self.list_attr[attr].set](indice);
 	return self.view();
 };
 MD.prototype.view = function(){
-	console.log('view');
 	const self = this;
 	Object.keys(self.list_attr).forEach(function(v){
 		self.list_dom[v].innerHTML = self.ts[self.list_attr[v]['get']]();
 	})
 	return this;
+};
+MD.prototype.createDOM = function(){
+	var self = this;
+	var yjsdate = document.createElement("div");
+	yjsdate.className = "yjsdate";
+	Object.keys(self.list_attr).forEach(function(v){
+		var e = document.createElement("div");
+		e.className = "e";
+		var u = document.createElement("div");
+		u.className = "up";
+		u.innerHTML = "+";
+		e.appendChild(u);
+		var v = document.createElement("div");
+		v.className = "val";
+		e.appendChild(v);
+		var d = document.createElement("div");
+		d.className = "down";
+		d.innerHTML = "-";
+		e.appendChild(d);
+		yjsdate.appendChild(e);
+	});
+	console.log('ms',yjsdate);
+	document.body.appendChild(yjsdate);
 };
 
 (function() {
